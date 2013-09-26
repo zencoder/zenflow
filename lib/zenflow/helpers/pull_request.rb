68da65ffadff65e56f2572f8a841662638ce15fb
Jon Kinney <jonkinney@gmail.com>
2013-09-26 15:53:43 -0500

    Update to a positive assertion

--- lib/zenflow/helpers/pull_request.rb
+++ lib/zenflow/helpers/pull_request.rb
@@ -13,7 +13,7 @@ module Zenflow
 
       def find_by_ref(ref, options={})
         Zenflow::Log("Looking up pull request for #{ref}") unless options[:silent]
-        if list != []
+        if list.any?
           pull = list.detect do |p|
             p["head"]["ref"] == ref
           end
