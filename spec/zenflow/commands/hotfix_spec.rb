f27948464b206b3dff70cf5e1ffa7412bb918247
Adam Kittelson <adam.kittelson@apathydrive.com>
2014-07-11 11:54:27 -0500

    fix specs

--- spec/zenflow/commands/hotfix_spec.rb
+++ spec/zenflow/commands/hotfix_spec.rb
@@ -5,7 +5,7 @@ describe Zenflow::Hotfix do
   subject { Zenflow::Hotfix.new }
 
   it { expect(subject.flow).to eq("hotfix") }
-  it { expect(subject.branch(:source)).to eq("production") }
+  it { expect(subject.branch(:source)).to be_false }
   it { expect(subject.branch(:deploy)).to match_array(["staging", "qa"]) }
   it { expect(subject.changelog).to eq(:rotate) }
   it { expect(subject.version).to eq(:patch) }
