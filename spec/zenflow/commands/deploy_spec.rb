6d3e08e48b901610540ae850642fe9900c514f71
Alejandra <acernas@brightcove.com>
2021-05-31 19:11:25 -0500

    Read trace option for better deploy outputs

--- spec/zenflow/commands/deploy_spec.rb
+++ spec/zenflow/commands/deploy_spec.rb
@@ -20,6 +20,15 @@ describe Zenflow::Deploy do
         Zenflow::Deploy('some-server')
       end
     end
+
+    context 'with trace' do
+      it 'deploys with trace' do
+        Zenflow::Branch.should_receive(:push).with('some-server')
+        Zenflow.should_receive(:Log).with("Deploying to some-server")
+        Zenflow::Shell.should_receive(:run).with("cap some-server deploy --trace")
+        Zenflow::Deploy('some-server', :trace => true)
+      end
+    end
   end
 
   subject {Zenflow::Deploy.new}
